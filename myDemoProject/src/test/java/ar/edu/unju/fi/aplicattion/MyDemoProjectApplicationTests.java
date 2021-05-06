48c8f8b3149bc43bd1fe10baca5208619858ab90
WIN 10 <WIN 10@DESKTOP-05HDK1R>
2021-05-06 05:34:11 -0300

    Responsable: Augusto Tolaba Tarea Realizada: Creacion del proyecto en STS

---
+++ myDemoProject/src/test/java/ar/edu/unju/fi/aplicattion/MyDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.aplicattion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
